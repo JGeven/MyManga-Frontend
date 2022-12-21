017e5122433b99e69fc2b4797c605cc8e7bbdfb5
JGeven <j.geven@student.fontys.nl>
2022-12-21 10:01:29 +0100

    Removed Project for gitignore

--- src/app/models/manga.ts
+++
@@ -1,8 +0,0 @@
-export class Manga {
-
-  mal_id: number;
-  title: string;
-  synopsis: string;
-
-  isFavorite: boolean = false;
-}
--- src/app/models/statistics.ts
+++
@@ -1,9 +0,0 @@
-export class Statistics {
-
-  reading: number;
-  completed: number;
-  on_hold: number;
-  dropped: number;
-  plan_to_read: number;
-  total: number;
-}
